7c73dc289b66ca07ffb13f59ba37efdbcc2887e8
sergioruizsan <srs.bcn@gmail.com>
2022-08-06 17:22:49 +0100

    part3 - 3.11: phonebook - serve frontend build from backend

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', (request, response) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
